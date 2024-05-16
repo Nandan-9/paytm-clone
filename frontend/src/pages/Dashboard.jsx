79d3fae3379452dde8ad517311ac578279bedbb9
Nandan_9 <a.d.devnandan@gmail.com>
2024-05-16 22:13:24 +0530

    pages

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+export default function Dashboard(){
+    return(
+        <>
+        <h1 className="text-3xl bg-slate-100 font-bold underline">
+      Hello world!
+        1</h1>
+
+        </>
+    )
+}
